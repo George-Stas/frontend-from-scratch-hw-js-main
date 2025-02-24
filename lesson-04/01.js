3b7a5461cc0d88a97f4d692276aad0e6f7cb05b7
George-Stas <goshastas19@gmail.com>
2025-02-24 22:03:44 +0300

    add les-4 task-1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,19 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+
+
+function includesElement(array, element) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === element){
+            return true
+        } 
+    } 
+    return false
+}
+
+const ar = includesElement([1, 2, 3], 2)
+
+console.log(ar);
+
+
