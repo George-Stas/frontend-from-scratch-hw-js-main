8fce1d10995211048ed41a2fb0e0ed6f72271cbd
George-Stas <goshastas19@gmail.com>
2025-03-12 21:59:29 +0300

    add les-6 task-1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -12,8 +12,40 @@
 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 */
 
-const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+document.addEventListener('DOMContentLoaded', function () {
+  const WEB_TECH_IMAGES = [
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+    'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+  ]
+
+  const slider = document.getElementById('web-tech-image');
+  // let srcValue = 0;
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+  const slideCount = WEB_TECH_IMAGES.length;
+  let slideIndex = 0;
+
+  // Устанавливаем обработчики событий для кнопок
+  prevButton.addEventListener('click',  function() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider();
+  });
+  nextButton.addEventListener('click', function() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+  });
+
+  // Функция для обновления отображения слайдера
+  function updateSlider() {
+    WEB_TECH_IMAGES.forEach((slide, index) => {
+      if (index === slideIndex) {
+        slider.src = slide
+      } else {
+      }
+    });
+  }
+
+  // Инициализация слайдера
+  updateSlider();
+});
